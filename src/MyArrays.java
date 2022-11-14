91eac9ca79cb1800288af3360d72dfd3afc36619
Ovadya33 <timurgts2710@gmail.com>
2022-11-14 22:01:14 +0300

    fixHomeWork 14.11.22

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -66,14 +66,12 @@ public class MyArrays {
 	 * @return index value if number exist otherwise -1 O[N] - search number in
 	 *         unsorted array O[LogN] - search number in sorted (binary search)
 	 */
-	
-	
-	
+
 	public static int binarySearch(int arraySorted[], int number) {
 		int left = 0;
 		int right = arraySorted.length - 1;
 		int middle = right / 2;
-		while(left <= right && arraySorted[middle] != number) {
+		while (left <= right && arraySorted[middle] != number) {
 			if (number < arraySorted[middle]) {
 				right = middle - 1;
 			} else {
@@ -81,108 +79,76 @@ public class MyArrays {
 			}
 			middle = (left + right) / 2;
 		}
-		
+
 		return left > right ? -1 : middle;
 	}
-	
-	
+
 	public static int binarySearchNew(int arraySorted[], int number) {
-			int left = 0;
-			int right = arraySorted.length - 1;
-			int middle = right / 2;
-			int res = -1;
-			while (left < right +1 ) {
-				if (number < arraySorted[middle]) {
-					right = middle - 1;
-				} else if (number == arraySorted[middle]) {
-					res = middle;
-					right = middle - 1;
-				}
-				else {
-					left = middle + 1;
-				}
-				middle = (left + right) / 2;
-			}
-			if (res == -1) {
-				res = -(left + 1);
-			}
-			return res;
-		}
-	
-		
-	public static boolean isOneSwapForSorted(int[] array) {
-		boolean res = false;
-		int first = 0;
-		int second = 0;
-		int сount = 0;
-		
-		for (int i = 1; i < array.length; i++) {
-			if (array[i] < array[i - 1]) {
-				if (сount == 0) {					
-					first = i - 1;
-					second = i;				
-				} else {					
-					second = i;	
-				}
-				сount ++;
-			}
-		}		
-		if (сount > 0 && сount < 3) {
-			res = checkSort(array, array[second], first) && checkSort(array, array[first], second);
-		} else {
-			res = false;
-		}		
-		return res;
-	}
-	
-	private static boolean checkSort(int[] array, int num, int pos) {
-		boolean res = false;
-		if (pos < array.length - 1 && pos > 0 ) {
-			if (num > (array[pos - 1] -1) && num < (array[pos + 1] +1)) {
-				res = true;
-			}
-		} else {
-			if (pos == 0) {
-				if (num < array[pos + 1]+1) {
-					res = true;
-				}
+		int left = 0;
+		int right = arraySorted.length - 1;
+		int middle = right / 2;
+		int res = -1;
+		while (left < right + 1) {
+			if (number < arraySorted[middle]) {
+				right = middle - 1;
+			} else if (number == arraySorted[middle]) {
+				res = middle;
+				right = middle - 1;
 			} else {
-				if (num > array[pos - 1]-1) {
-					res = true;
-				}
+				left = middle + 1;
 			}
+			middle = (left + right) / 2;
+		}
+		if (res == -1) {
+			res = -(left + 1);
 		}
 		return res;
 	}
-	
-	
 
-	
-	public static int[] bubbleSort(int[] array) {		
-			
-			boolean isSorted = false;
-			while (!isSorted) {				
-				isSorted = true;
-				for (int i = 1; i < array.length; i++) {
-					if (array[i] < array[i - 1]) {
-						swap(array, i - 1, i);
-						isSorted = false;
-					}
-				}
-				for (int i = array.length - 1; i > 0; i--) {
-					if (array[i] < array[i - 1]) {
-						swap(array, i - 1, i);
-						isSorted = false;
-					}
-				}				
+	public static boolean isOneSwapForSorted(int array[]) {
+
+		int сount = 0;
+		int sort[] = new int[array.length];
+		System.arraycopy(array, 0, sort, 0, array.length);
+		sort = bubbleSort(sort);
+		for (int i = 0; i < array.length; i++) {
+			if (sort[i] != array[i]) {
+				сount++;
 			}
-			return array;
-			
+		}
+		if (сount == 2) {
+			return true;
+		} else {
+			return false;
 		}
 
-		public static void swap(int array[], int index1, int index2) {
-			int buffer = array[index1];
-			array[index1] = array[index2];
-			array[index2] = buffer;
+	}
+
+	public static int[] bubbleSort(int[] array) {
+
+		boolean isSorted = false;
+		while (!isSorted) {
+			isSorted = true;
+			for (int i = 1; i < array.length; i++) {
+				if (array[i] < array[i - 1]) {
+					swap(array, i - 1, i);
+					isSorted = false;
+				}
+			}
+			for (int i = array.length - 1; i > 0; i--) {
+				if (array[i] < array[i - 1]) {
+					swap(array, i - 1, i);
+					isSorted = false;
+				}
+			}
 		}
+		return array;
+
+	}
+
+	public static void swap(int array[], int index1, int index2) {
+		int buffer = array[index1];
+		array[index1] = array[index2];
+		array[index2] = buffer;
+	}
 }
