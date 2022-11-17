9b44df3dffa68499f05141b9417c106865291b65
Ovadya33 <timurgts2710@gmail.com>
2022-11-17 18:21:40 +0300

    classwork 17.11.22

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+	/**
+	 * 
+	 * @param str
+	 * @param anagram
+	 * @return true if str2 is an anagram of str1
+	 * complexity 0[N] two passes
+	 * 1. the same length
+	 * 2. the same symbols just in different order
+	 */
+public static boolean isAnagram(String str1, String str2) {
+	return false;
+}
+}
